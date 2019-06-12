3250b7d935f033c6fd2abfc67b943709aa289697
marco.pierre <marco.pierre@comsave.com>
2019-06-12 16:47:53 +0200

    min_membership refactored

--- selection-sort.thy
+++ selection-sort.thy
@@ -2,13 +2,27 @@ theory "selection-sort"
   imports Main "HOL-Library.Multiset"
 begin
 
-lemma p_100 [simp]: "m = Min(set (x#xs)) \<Longrightarrow> m \<in> set (x#xs)"
-proof(induct xs arbitrary: x)
+lemma min_membership: "m = Min(set (x#xs)) \<Longrightarrow> m \<in> set (x#xs)"
+proof(induct xs arbitrary: x m)
   case Nil
   then show ?case by simp
 next
   case (Cons a xs)
-  then show ?case using eq_Min_iff by blast
+  then show "m \<in> set (x # a # xs)"
+  proof(cases "m = Min (set (a#xs))")
+    case True
+    have "m \<in> set (a# xs)" using Cons.hyps True by simp
+    moreover have "m \<in> set (x # a # xs)" using calculation by auto
+    then show "m \<in> set (x # a # xs)" by assumption
+  next
+    case False
+    have "m \<in> {Min ({x} \<union> set (a # xs))}" by (metis Cons.prems insertCI insert_is_Un list.simps(15))
+    moreover have "m \<notin> set (a # xs)" by (metis Cons.prems False List.finite_set Min_eqI Min_le insertI2 list.simps(15))
+    moreover have "m \<in> {Min ({x})}" by (metis (mono_tags, lifting) List.finite_set Min_in calculation(1) calculation(2) insert_iff insert_is_Un list.simps(15) set_empty singleton_iff)
+    moreover have "m \<in> {x}" using Min_singleton calculation(3) by simp
+    moreover have  "m \<in> set (x # a # xs)"  using calculation(4) by auto
+    then show "m \<in> set (x # a # xs)" by assumption
+  qed
 qed
 
 lemma p_3 [simp]: "y \<in> set (x#xs) \<Longrightarrow> length (remove1 y (x#xs)) = length (x#xs) - 1"
@@ -33,7 +47,7 @@ function selection_sort:: "nat list \<Rightarrow> nat list" where
 "selection_sort [] = []" |
 "selection_sort (x#xs) = (let m = Min (set(x#xs)) in m#selection_sort(remove1 m (x#xs)))"
 by pat_completeness auto
-termination by (meson "termination" in_measure p_100 p_5 wf_measure)
+termination by (meson "termination" in_measure min_membership p_5 wf_measure)
 
 value "selection_sort [2,4,10,0,0]"
 (*
@@ -99,7 +113,7 @@ case 1
 then show ?case by simp
 next
   case (2 x xs)
-  then show ?case by (metis "100" mset.simps(1) mset.simps(2) mset_append mset_rev p_100 rev.simps(2) selection_sort.simps(2))
+  then show ?case by (metis "100" add.right_neutral min_membership mset.simps(2) selection_sort.simps(2) union_mset_add_mset_right)
 qed
 
 
