634d4302b53c287900f37976b2144cd9ba4427dc
marco.pierre <marco.pierre@comsave.com>
2019-07-20 10:29:13 +0200

    using strucural indcution for this one

--- insertion-sort.thy
+++ insertion-sort.thy
@@ -4,13 +4,13 @@ begin
 
 declare[[names_short]]
 
-fun insert:: "nat \<Rightarrow> nat list \<Rightarrow> nat list" where
+primrec insert:: "nat \<Rightarrow> nat list \<Rightarrow> nat list" where
 insert_Nil: "insert x [] = [x]" |
 insert_Cons: "insert x (y#ys) = (if x < y then (x#y#ys) else y#insert x ys)"
 
 value "insert 1 [2,4,10]"
 
-fun insertion_sort:: "nat list \<Rightarrow> nat list" where
+primrec insertion_sort:: "nat list \<Rightarrow> nat list" where
 insertion_sort_Nil : "insertion_sort []  = []" |
 insertion_sort_Cons: "insertion_sort (x#xs)  = insert x (insertion_sort(xs))"
 
@@ -25,43 +25,43 @@ next
   then show ?case by (simp del:List.linorder_class.sorted.simps add:sorted2_simps)
 qed
 
-lemma insert_order: "sorted(ys) \<Longrightarrow> sorted (insert y ys)"
-proof (induct ys  rule: insert.induct)
-  case (1 x)
+lemma insert_order: "sorted(ys) \<Longrightarrow> sorted (insert x ys)"
+proof (induct ys arbitrary: x)
+  case Nil
   then show "sorted (insert x [])" by simp
 next
-  case (2 x y ys)
-  then show "sorted (insert x (y # ys))"
+  case (Cons y ys)
+  then show "sorted (insert x (y # ys))" 
   proof (cases "x < y")
     case True
-    then show "sorted (insert x (y#ys))"
+    then show "sorted (insert x (y # ys))"
     proof (simp only: True insert_Cons if_True)
       show "sorted (x # y # ys)"
       proof(simp)
-        show "x \<le> y \<and> (\<forall>xa\<in>set ys. x \<le> xa) \<and> (\<forall>x\<in>set ys. y \<le> x) \<and> sorted ys"
+        show "x \<le> y \<and> Ball (set ys) ((\<le>) x) \<and> Ball (set ys) ((\<le>) y) \<and> sorted ys"
         proof(intro conjI)
-          show "x \<le> y" by (simp add: True le_less)
+          show "x \<le> y"  by (simp add: Orderings.order_class.order.strict_implies_order True)
         next
-          show "\<forall>xa\<in>set ys. x \<le> xa" using "local.2.prems" True by auto
+          show "Ball (set ys) ((\<le>) x)" using True local.Cons.prems by auto
         next
-          show "(\<forall>x\<in>set ys. y \<le> x)" using "local.2.prems" List.linorder_class.sorted.simps(2) by blast
+          show "Ball (set ys) ((\<le>) y)"  using List.linorder_class.sorted.simps(2) local.Cons.prems by simp
         next
-          show "sorted ys" using "local.2.prems" List.linorder_class.sorted.simps(2) by blast
+          show "sorted ys" using List.linorder_class.sorted.simps(2) local.Cons.prems by simp
         qed
       qed
     qed
   next
     case False
-    then show "sorted (insert x (y # ys))"
+    then show "sorted (insert x (y # ys))" 
     proof(simp only:False insert_Cons if_False)
       show "sorted (y # insert x ys)"
-      proof(simp  del:List.linorder_class.sorted.simps add: False sorted3 "local.2.prems")
-        show "y \<le> x \<and> sorted (insert x ys)" 
+      proof(simp  del:List.linorder_class.sorted.simps add: False sorted3 "local.Cons.prems")
+        show "y \<le> x \<and> sorted (insert x ys)"
         proof(rule conjI)
-          show "y \<le> x" by (simp add: False leI)
+          show "y \<le> x"  by (simp add: False leI)
         next
-          have "sorted ys" using "local.2.prems" List.linorder_class.sorted.simps(2) by blast
-          then show "sorted (insert x ys)" by (simp add: "local.2.hyps" False)
+          have "sorted ys" using "local.Cons.prems" List.linorder_class.sorted.simps(2) by blast
+          then show "sorted (insert x ys)"  by (simp add: local.Cons.hyps)
         qed
       qed
     qed
@@ -69,48 +69,48 @@ next
 qed
 
 theorem insertion_sort_order : "sorted(insertion_sort(ys))"
-proof (induct ys rule:insertion_sort.induct)
-  case 1
+proof (induct ys)
+  case Nil
   then show "sorted (insertion_sort [])" by simp
 next
-  case (2 x xs)
-  show "sorted (insertion_sort (x # xs))"
+  case (Cons y ys)
+  show "sorted (insertion_sort (y # ys))"
   proof (simp only: insertion_sort_Cons)
-    show "sorted (insert x (insertion_sort xs))" by (simp only: "local.2.hyps" insert_order)
+    show "sorted (insert y (insertion_sort ys))" by (simp only: "local.Cons.hyps" insert_order)
   qed
 qed
 
-lemma insert_permutation: "mset (insert y ys) = mset (y#ys)"
-proof(induct ys rule:insert.induct)
-  case (1 x)
-  show "mset (insert x []) = mset [x]" by simp
+lemma insert_permutation: "mset (insert x ys) = mset (x#ys)"
+proof(induct ys arbitrary: x)
+  case Nil
+  then show "mset (insert x []) = mset [x]" by simp
 next
-  case (2 x y ys)
-  then show ?case
+  case (Cons y ys)
+  then show "mset (insert x (y # ys)) = mset (x # y # ys)"
   proof (cases "x < y")
     case True
-    then show "mset (insert x (y # ys)) = mset (x # y # ys)"  by simp
+    then show "mset (insert x (y # ys)) = mset (x # y # ys)" by simp
   next
     case False
     have "mset (insert x (y # ys)) = mset (y#insert x ys)" using False by simp
     also have "... = {#y#} + mset(insert x ys)" by simp
-    also have "... = {#y#} + mset (x # ys)" using "local.2.hyps" False by simp
+    also have "... = {#y#} + mset (x # ys)" using "local.Cons.hyps" False by simp
     also have "... = mset (x # y # ys)"  by simp
     finally show "mset (insert x (y # ys)) = mset (x # y # ys)" by this
   qed
 qed
 
-theorem insert_sort_permutation: "mset (insertion_sort ys) = mset ys"
-proof(induct ys rule: insertion_sort.induct)
-  case 1
+theorem insertion_sort_permutation: "mset (insertion_sort ys) = mset ys"
+proof(induct ys)
+  case Nil
   then show "mset (insertion_sort []) = mset []" by simp
 next
-  case (2 x xs)
+  case (Cons x xs)
   have "mset (insertion_sort (x # xs)) = mset (insert x (insertion_sort(xs)))" by simp
   also have "... =  mset(x#(insertion_sort(xs)))" using  insert_permutation by simp
   also have "... =  {#x#} + mset(insertion_sort(xs))" by simp
-  also have "... =  {#x#} +  mset xs" using "local.2.hyps" by simp
-  also have "... =  mset (x # xs)" using "local.2.hyps" by simp
+  also have "... =  {#x#} +  mset xs" using "local.Cons.hyps" by simp
+  also have "... =  mset (x # xs)" using "local.Cons.hyps" by simp
   finally show "mset (insertion_sort (x # xs)) = mset (x # xs)" by this
 qed
 
